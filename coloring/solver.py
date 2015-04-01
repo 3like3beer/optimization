02bfdee7661b89a67c850128399ce68f3b344de6
julien <les.ruiz@gmail.com>
2015-04-01 21:56:51 +0200

    regle

--- coloring/solver.py
+++ coloring/solver.py
@@ -23,10 +23,13 @@ def solve_it(input_data):
         edges.append((int(parts[0]), int(parts[1])))
     # build a trivial solution
     # every node has its own color
-    solution = range(0, node_count)
-    pulp_solve(node_count,edges)
+    #solution = range(0, node_count)
+
+
+    solution = pulp_solve(node_count,edges)
+
     # prepare the solution in the specified output format
-    output_data = str(node_count) + ' ' + str(0) + '\n'
+    output_data = str(max(solution)+1) + ' ' + str(0) + '\n'
     output_data += ' '.join(map(str, solution))
 
     return output_data
@@ -35,7 +38,7 @@ def pulp_solve(node_count,edges):
     coloring = pulp.LpProblem("Color Model", pulp.LpMinimize)
     color_set = range(0,node_count)
     is_color =  [[pulp.LpVariable("x_col" + str(color) + "_node" + str(node) , 0,1, 'Binary') for color in color_set] for node in color_set]
-    obj = pulp.LpVariable("objective",0,node_count,'Integer')
+    obj = pulp.LpVariable("objective",node_count/5,node_count,'Integer')
     objective = pulp.LpAffineExpression(obj)
     coloring.setObjective(objective)
 
@@ -44,15 +47,18 @@ def pulp_solve(node_count,edges):
             coloring += node * is_color[color][node] <= obj
         coloring += sum(is_color[color][v] for v in color_set) == 1
         for e in edges:
-            coloring += is_color[color][e[0]] + is_color[color][e[1]] <= 1
-    print(coloring)
-    coloring.solve(pulp.COIN_CMD())
+            coloring += is_color[e[0]][color] + is_color[e[1]][color] <= 1
+    #print(coloring)
+    coloring.solve(pulp.GLPK_CMD())
 
-    for color in color_set:
-        for node in color_set:
-            print(is_color[color][node])
-            print(is_color[color][node].value())
-    return 0
+    out = []
+    for node in color_set:
+        for color in color_set:
+            if is_color[node][color].value()>0.5:
+                #print ("col" + str(color) + "node" + str(node))
+                out.append(color)
+    #print out
+    return out
 
 import sys
 
