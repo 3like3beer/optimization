cc46579ef5d082df150c746e2ff0faeae46800c2
julien <les.ruiz@gmail.com>
2015-04-03 21:49:24 +0200

    regle

--- tsp/solver.py
+++ tsp/solver.py
@@ -25,7 +25,7 @@ def solve_it(input_data):
         points.append(Point(float(parts[0]), float(parts[1])))
 
     solution = pulp_solution(points,node_count)
-
+    print(len(solution))
     # calculate the length of the tour
     obj = length(points[solution[-1]], points[solution[0]])
     for index in range(0, node_count-1):
@@ -46,14 +46,23 @@ def build_variable(node_in,node_out):
     if node_in > node_out:
         return pulp.LpVariable("x_in" + str(node_in) + "_out" + str(node_out) , 0,1, 'Binary')
     else:
-        return 0
+        return 0.0
 
-def find_out_node(node_in,out_edges,node_set):
+def find_out_node(node_in,out_edges,node_set,visited):
+    #print ("input " + str(node_in))
     for node_out in node_set:
-        if node_out > node_in:
-            if out_edges[node_in][node_out].value()>0.7:
-                return node_out
-    #return 0
+        if out_edges[node_in][node_out] != 0.0:
+            if not(visited[node_in][node_out]):
+                if out_edges[node_in][node_out].value()>0.7:
+                    #print ("out1 " + str(out_edges[node_in][node_out]))
+                    return node_out,1
+
+        if out_edges[node_out][node_in] != 0.0:
+            if not(visited[node_out][node_in]):
+                if out_edges[node_out][node_in].value()>0.7:
+                    #print ("out2 " + str(out_edges[node_out][node_in]))
+                    return node_out,2
+    return None,0
 
 def pulp_solution(points,node_count):
     tsp = pulp.LpProblem("Tsp Model", pulp.LpMinimize)
@@ -66,20 +75,30 @@ def pulp_solution(points,node_count):
     tsp+= sum([sum([out_edges[node_in][node_out]*length(points[node_in], points[node_out]) for node_in in node_set ]) for node_out in node_set])
 
     for node in node_set:
-        tsp += sum(out_edges[node][v] for v in node_set)  + sum(out_edges[v][node] for v in node_set) == 2
+        tsp += sum(out_edges[node][v] for v in node_set) + sum(out_edges[v][node] for v in node_set) == 2
 
     tsp.solve()
 
     for t in tsp.variables():
-        print (str(t) + " " + str(t.value()))
+        if t.value()>0.5:
+            print (str(t) + " " + str(t.value()))
+
     out = [0]
+    visited = [[False for node_in in node_set] for node_out in node_set]
     node_in = 0
-    while node_in != None:
-        node_out = find_out_node(node_in,out_edges,node_set)
-        node_in = node_out
-        if node_in != None:
+    while node_in is not None:
+        node_out,a = find_out_node(node_in,out_edges,node_set,visited)
+        if node_out is not None:
             out.append(node_out)
-    print out
+            if a == 1:
+                visited[node_in][node_out] = True
+                #print ("visited1 " + str(node_in) + " " +str(node_out))
+            else:
+                visited[node_out][node_in] = True
+                #print ("visited2 " + str(node_out) + " "  + str(node_in))
+        node_in = node_out
+    print(out)
+    print(sorted(out))
     return out
 
 import sys
