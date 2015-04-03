24af14e3b4bc81ccc2d98200890ff9985d65ef52
julien <les.ruiz@gmail.com>
2015-04-03 17:30:58 +0200

    regle

--- tsp/solver.py
+++ tsp/solver.py
@@ -24,9 +24,7 @@ def solve_it(input_data):
         parts = line.split()
         points.append(Point(float(parts[0]), float(parts[1])))
 
-    # build a trivial solution
-    # visit the nodes in the order they appear in the file
-    solution = range(0, nodeCount)
+    solution = pulp_solution(points,node_count)
 
     # calculate the length of the tour
     obj = length(points[solution[-1]], points[solution[0]])
@@ -39,30 +37,28 @@ def solve_it(input_data):
 
     return output_data
 
+def trivial_solution(points,node_count):
+    # build a trivial solution
+    # visit the nodes in the order they appear in the file
+    return range(0, nodeCount)
 
 def pulp_solution(points,node_count):
     tsp = pulp.LpProblem("Tsp Model", pulp.LpMinimize)
     node_set = range(0,node_count)
-    out_edges =  [[pulp.LpVariable("x_col" + str(color) + "_node" + str(node) , 0,1, 'Binary') for color in range(0,node)] for node in node_set]
+    out_edges =  [[pulp.LpVariable("x_in" + str(node_in) + "_out" + str(node_out) , 0,1, 'Binary') for node_in in node_set if  node_in not in [node_out]] for node_out in node_set]
     #objective = pulp.LpAffineExpression([ (out_edges[i,k],i.value) for i in node_set for k in node_set))
-    tsp+= sum([sum([out_edges[i,j]*length(points(i), points(j)) for i in range(0,j)]) for j in node_set])
+    tsp+= sum([sum([out_edges[node_in][node_out]*length(points(node_in), points(node_out)) for node_in in node_set]) for node_out in node_set])
 
-    for color in node_set:
-        for node in node_set:
-            tsp += node * out_edges[color][node] <= obj
-        tsp += sum(out_edges[color][v] for v in node_set) == 1
-        for e in edges:
-            tsp += out_edges[e[0]][color] + out_edges[e[1]][color] <= 1
-    #print(coloring)
+    for node in node_set:
+        tsp += sum(out_edges[node][v] for v in node_set) == 1
+        tsp += sum(out_edges[v][node] for v in node_set) == 1
     tsp.solve(pulp.GLPK_CMD())
 
-    out = []
-    for node in node_set:
-        for color in node_set:
-            if out_edges[node][color].value()>0.5:
-                #print ("col" + str(color) + "node" + str(node))
-                out.append(color)
-    #print out
+    out = [0]
+    for node_out in node_set:
+        for node_in in node_set:
+            if out_edges[node_out][node_in].value()>0.5:
+                out.append(node_in)
     return out
 
 import sys
