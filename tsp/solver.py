f0ed9501dfe43fc27981b37e1f0db3933646ea89
julien <les.ruiz@gmail.com>
2015-04-03 18:37:08 +0200

    regle

--- tsp/solver.py
+++ tsp/solver.py
@@ -16,10 +16,10 @@ def solve_it(input_data):
     # parse the input
     lines = input_data.split('\n')
 
-    nodeCount = int(lines[0])
+    node_count = int(lines[0])
 
     points = []
-    for i in range(1, nodeCount+1):
+    for i in range(1, node_count+1):
         line = lines[i]
         parts = line.split()
         points.append(Point(float(parts[0]), float(parts[1])))
@@ -28,7 +28,7 @@ def solve_it(input_data):
 
     # calculate the length of the tour
     obj = length(points[solution[-1]], points[solution[0]])
-    for index in range(0, nodeCount-1):
+    for index in range(0, node_count-1):
         obj += length(points[solution[index]], points[solution[index+1]])
 
     # prepare the solution in the specified output format
@@ -40,25 +40,46 @@ def solve_it(input_data):
 def trivial_solution(points,node_count):
     # build a trivial solution
     # visit the nodes in the order they appear in the file
-    return range(0, nodeCount)
+    return range(0, node_count)
+
+def build_variable(node_in,node_out):
+    if node_in > node_out:
+        return pulp.LpVariable("x_in" + str(node_in) + "_out" + str(node_out) , 0,1, 'Binary')
+    else:
+        return 0
+
+def find_out_node(node_in,out_edges,node_set):
+    for node_out in node_set:
+        if node_out > node_in:
+            if out_edges[node_in][node_out].value()>0.7:
+                return node_out
+    #return 0
 
 def pulp_solution(points,node_count):
     tsp = pulp.LpProblem("Tsp Model", pulp.LpMinimize)
     node_set = range(0,node_count)
-    out_edges =  [[pulp.LpVariable("x_in" + str(node_in) + "_out" + str(node_out) , 0,1, 'Binary') for node_in in node_set if  node_in not in [node_out]] for node_out in node_set]
+    out_edges =  [[build_variable(node_in,node_out) for node_in in node_set] for node_out in node_set]
     #objective = pulp.LpAffineExpression([ (out_edges[i,k],i.value) for i in node_set for k in node_set))
-    tsp+= sum([sum([out_edges[node_in][node_out]*length(points(node_in), points(node_out)) for node_in in node_set]) for node_out in node_set])
+    #out_edges2 =  [[1 for node_in in node_set] for node_out in node_set]
+    #print([out_edges2[node_out] for node_out in node_set])
+    #print([[out_edges2[node_in][node_out]*length(points[node_in], points[node_out]) for node_in in node_set ] for node_out in node_set])
+    tsp+= sum([sum([out_edges[node_in][node_out]*length(points[node_in], points[node_out]) for node_in in node_set ]) for node_out in node_set])
 
     for node in node_set:
-        tsp += sum(out_edges[node][v] for v in node_set) == 1
-        tsp += sum(out_edges[v][node] for v in node_set) == 1
-    tsp.solve(pulp.GLPK_CMD())
+        tsp += sum(out_edges[node][v] for v in node_set)  + sum(out_edges[v][node] for v in node_set) == 2
 
+    tsp.solve()
+
+    for t in tsp.variables():
+        print (str(t) + " " + str(t.value()))
     out = [0]
-    for node_out in node_set:
-        for node_in in node_set:
-            if out_edges[node_out][node_in].value()>0.5:
-                out.append(node_in)
+    node_in = 0
+    while node_in != None:
+        node_out = find_out_node(node_in,out_edges,node_set)
+        node_in = node_out
+        if node_in != None:
+            out.append(node_out)
+    print out
     return out
 
 import sys
