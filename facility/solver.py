639783e79915d5b28152d83a5733965f797bf942
Julien <les.ruiz@gmail.com>
2015-04-21 18:44:57 +0200

    greedy

--- facility/solver.py
+++ facility/solver.py
@@ -2,6 +2,7 @@
 # -*- coding: utf-8 -*-
 
 from collections import namedtuple
+from random import shuffle
 import math
 import pulp
 
@@ -11,7 +12,7 @@ Customer = namedtuple("Customer", ['index', 'demand', 'location'])
 
 
 def length(point1, point2):
-    return math.sqrt((point1.x - point2.x)**2 + (point1.y - point2.y)**2)
+    return math.sqrt((point1.x - point2.x) ** 2 + (point1.y - point2.y) ** 2)
 
 
 def cost(customer, facility):
@@ -33,30 +34,42 @@ def create_use_list(facilities, solution):
         used[facility_index] = 1
     return used
 
+
 def get_unconnected(solution):
-    return [i for i,c in enumerate(solution) if c == -1 ]
+    return [i for i, c in enumerate(solution) if c <= -0.5]
+
 
 # At every moment, each customer j offers some money from its budget to each unopened facility i.
 def get_offer(customer, facilities, facility, B, solution):
-    offer2 = cost(customer,facility)
+    offer2 = cost(customer, facility)
     if (solution[customer.index] == -1):
         # The amount of this offer is equal to max(Bj −cij,0) if j is unconnected,
         offer1 = B[customer.index]
     else:
         # and max(ci′j − cij,0) if it is connected to some other facility i′.
-        offer1 = cost(customer , facilities[solution[customer.index]])
-    return  max(offer1-offer2,0)
+        offer1 = cost(customer, facilities[solution[customer.index]])
+    return max(offer1 - offer2, 0)
 
 
-def open_facility(f, a, solution):
-    print "Opening Facility " + str(f.index)
-    for offer,c in a:
-        if offer > 0:
+def open_facility(f, assigned, solution, capacustomer_remaining):
+    if assigned:
+        for c in assigned:
+            if solution[c.index] > -1:
+                capacustomer_remaining[solution[c.index]] += c.demand
             solution[c.index] = f.index
-    print "New solution " + str(solution)
+            capacustomer_remaining[f.index] -= c.demand
+        #print "Opening Facility " + str(f.index) + " assigning " + to_string(
+        #    [c.index for c in assigned]) + " To assign " + to_string(get_unconnected(solution))
 
 
-def open_if_possible(customers, facilities, f, B, solution):
+def to_string(list):
+    if len(list) > 10:
+        return "list of size: " + str(len(list))
+    else:
+        return str(list)
+
+
+def open_if_possible(customers, facilities, f, B, solution, capacustomer_remaining):
     total_offer = 0
     offers = []
     for c in customers:
@@ -67,55 +80,154 @@ def open_if_possible(customers, facilities, f, B, solution):
         return
     else:
         total_offer = 0
-        a = zip(offers,customers)
-        a.sort(key = lambda x:x[0], reverse = True)
+        a = zip(offers, customers)
+        a.sort(key=lambda x: x[0], reverse=True)
         capacity = f.capacustomer
         assigned = []
-        while total_offer < f.setup_cost:
-            for offer,c in a:
-                if capacity <= 0:
-                    return
-                else:
+        while total_offer < f.setup_cost and capacity >= c.demand:
+            for offer, c in a:
+                if capacity >= c.demand:
                     assigned.append(c)
-                    capacity -= 1
+                    capacity -= c.demand
                     total_offer += offer
-                    #fin
-        open_facility(f, a, solution)
+        open_facility(f, assigned, solution, capacustomer_remaining)
+
+
+def get_demand(customers, f, solution):
+    demand = 0
+    for c in customers:
+        if solution[c.index] == f.index:
+            demand += c.demand
+    return demand
+
+
+def test_swap(open_f, closed_f, customers, facilities, solution):
+    new_solution = [s for s in solution]
+    cur_cost = solution_cost(customers, facilities, solution, create_use_list(facilities, solution))
+    demand = get_demand(customers, open_f, solution)
+    if demand < closed_f.capacustomer:
+        #print "test swaping " + str(open_f.index) + " and " + str(closed_f.index)
+        for c in customers:
+            if new_solution[c.index] == open_f.index:
+                new_solution[c.index] = closed_f.index
+        assert create_use_list(facilities, new_solution)[open_f.index] == 0
+        #assert create_use_list(facilities, new_solution)[closed_f.index] == 1
+    if solution_cost(customers, facilities, new_solution, create_use_list(facilities, new_solution)) < cur_cost:
+        #print "new cost " + str(solution_cost(customers, facilities, new_solution, create_use_list(facilities, new_solution)))
+        return new_solution,create_use_list(facilities, new_solution)
+    else:
+        return solution,create_use_list(facilities, solution)
+
+
+def opt_local(customers, facilities, min_cost, min_solution, min_used):
+    old_cost = 10000000000000000
+    while min_cost < old_cost:
+        old_cost = min_cost
+        for open_f in [f for f in facilities if min_used[f.index] > 0.5]:
+            for closed_f in [f for f in facilities if min_used[f.index] < 0.5]:
+                solution, used = test_swap(open_f, closed_f, customers, facilities, min_solution)
+                for c in customers:
+                    for old_f in [f for f in facilities if min_used[f.index] > 0.5]:
+                        for new_f in [f for f in facilities if min_used[f.index] > 0.5]:
+                            if solution[c.index] == old_f and new_f.capacustomer > c.demand + get_demand(customers, f, solution):
+                                new_sol = [s for s in solution]
+                                new_sol[c.index] = new_f
+                                if solution_cost(customers, facilities, new_sol, create_use_list(facilities, new_sol)) < cur_cost:
+                                    solution = new_sol
+                                    used = create_use_list(facilities, new_sol)
+                cur_cost = solution_cost(customers, facilities, solution, used)
+                if cur_cost < min_cost:
+                    min_cost = cur_cost
+                    min_solution = solution
+                    min_used = used
+    return min_solution, min_used
+
+
+def local_greedy2(customers, facilities):
+    min_solution, min_used = local_greedy(customers, facilities)
+    min_cost = solution_cost(customers, facilities, min_solution, min_used)
+    print "shuffle_greedy cost " + str(min_cost)
+    min_solution, min_used = opt_local(customers, facilities, min_cost, min_solution, min_used)
+    return min_solution, min_used
+
+
+
+def local_greedy(customers, facilities):
+    nb = 500
+    i = 0
+    min_cost = 10000000000000000
+    while i < nb:
+        solution, used = greedy_solution(customers, facilities)
+        cur_cost = solution_cost(customers, facilities, solution, used)
+        if cur_cost < min_cost:
+            min_cost = cur_cost
+            min_solution = solution
+            min_used = used
+            print str(min_cost)
+        i += 1
+    return min_solution, min_used
+
 
 def greedy_solution(customers, facilities):
-    rate = 10000
-    #1. At the beginning, all customers are unconnected,
+    rate = 1000
+    # 1. At the beginning, all customers are unconnected,
     solution = [-1] * len(customers)
     # all facilities are unopened,
     # and the budget of every customer j, denoted by Bj, is initialized to 0.
     B = [0 for c in customers]
 
-    #2. While there is an unconnected customer,
+    capacustomer_remaining = [f.capacustomer for f in facilities]
+
+    # 2. While there is an unconnected customer,
     unconnected = get_unconnected(solution)
     while unconnected:
-        print "unconnected " + str(unconnected)
         used = create_use_list(facilities, solution)
         # increase the budget of each unconnected customer at the same rate,
-        for uc in unconnected:
-        # until one of the following events occurs:
-            for f in facilities:
-                if used[f.index] == 0:
-                    # (a) For some unopened facility i, the total offer that it receives from customers is equal to the cost of opening i.
-                    # In this case, we open facility i,
-                    # and for every customer j (connected or unconnected) which has a non-zero offer to i, we connect j to i.
-                    open_if_possible(customers, facilities, f, B, solution)
-                else:
-                    #(b) For some unconnected customer j,
-                    # and some facility i that is already open,
-                    # the budget of j is equal to the connection cost cij.
-                    # In this case, we connect j to i.
-                    if B[uc] >= cost(customers[uc] ,f):
-                        solution[uc] = f.index
-            B[uc] += rate
-        unconnected = get_unconnected(solution)
-        #print B
+        while unconnected:
+            for uc in unconnected:
+                B[uc] += rate
+                # until one of the following events occurs:
+                shuffle_facilities = [f for f in facilities]
+                shuffle(shuffle_facilities)
+                for f in shuffle_facilities:
+                    used = create_use_list(facilities, solution)
+                    if used[f.index] < 1:
+                        #and f.index<> 1 and f.index<> 16 and f.index<> 22 and f.index<> 29 and f.index<> 55 and f.index<> 61
+                        # (a) For some unopened facility i, the total offer that it receives from customers is equal to the cost of opening i.
+                        # In this case, we open facility i,
+                        # and for every customer j (connected or unconnected) which has a non-zero offer to i, we connect j to i.
+                        open_if_possible(customers, facilities, f, B, solution, capacustomer_remaining)
+                        unconnected = get_unconnected(solution)
+                        if not unconnected:
+                            used = create_use_list(facilities, solution)
+                            return opt_local(customers, facilities, solution_cost(customers, facilities, solution, used) , solution, used)
+                            #print ("unconnected 1 " + to_string(unconnected))
+                    else:
+                        unconnected = get_unconnected(solution)
+                        shuffle_unconnected = [c for c in unconnected]
+                        shuffle(shuffle_unconnected)
+                        #print ("unconnected 2 " + to_string(unconnected))
+                        for uc in shuffle_unconnected:
+                            #(b) For some unconnected customer j,
+                            # and some facility i that is already open,
+                            # the budget of j is equal to the connection cost cij.
+                            # In this case, we connect j to i.
+                            if solution[uc] < -0.5:
+                                if B[uc] >= 0:
+                                    if capacustomer_remaining[f.index] > customers[uc].demand:
+                                        #print "connect customer " + str(uc) + " to facility " + str(f.index)
+                                        #print "Current solution " + str(solution)
+                                        solution[uc] = f.index
+                                        capacustomer_remaining[f.index] -= customers[uc].demand
+                        if not unconnected:
+                            used = create_use_list(facilities, solution)
+                            return opt_local(customers, facilities, solution_cost(customers, facilities, solution, used) , solution, used)
+            unconnected = get_unconnected(solution)
+            #print ("unconnected " + to_string(unconnected))
+            #print B
     used = create_use_list(facilities, solution)
-    return solution, used
+    return opt_local(customers, facilities, solution_cost(customers, facilities, solution, used) , solution, used)
+
 
 
 def trivial_solution(customers, facilities):
@@ -123,41 +235,47 @@ def trivial_solution(customers, facilities):
     # pack the facilities one by one until all the customers are served
     solution = [-1] * len(customers)
     capacustomer_remaining = [f.capacustomer for f in facilities]
-    facility_index = 0
+    facilities_index = [f.index for f in facilities]
+    shuffle(facilities_index)
+    i = 0
+    facility_index = facilities_index[i]
     for customer in customers:
         if capacustomer_remaining[facility_index] >= customer.demand:
             solution[customer.index] = facility_index
             capacustomer_remaining[facility_index] -= customer.demand
         else:
-            facility_index += 1
+            i += 1
+            facility_index = facilities_index[i]
             assert capacustomer_remaining[facility_index] >= customer.demand
             solution[customer.index] = facility_index
             capacustomer_remaining[facility_index] -= customer.demand
     used = create_use_list(facilities, solution)
     return solution, used
 
-def pulp_solve(node_count,edges,opt):
+
+def pulp_solve(node_count, edges, opt):
     facility = pulp.LpProblem("Facility Model", pulp.LpMinimize)
-    f_set = range(0,node_count)
-    used =  [pulp.LpVariable("f" + str(f)  , 0,1, 'Binary') for f in f_set]
+    f_set = range(0, node_count)
+    used = [pulp.LpVariable("f" + str(f), 0, 1, 'Binary') for f in f_set]
     # obj = pulp.LpVariable("objective",1,opt+1,'Integer')
     facility += sum([f.setup_cost * used[f.index] for f in f_set])
-    obj = 1 #sum([f.setup_cost * used[f.index] for f in facilities])
+    obj = 1  # sum([f.setup_cost * used[f.index] for f in facilities])
     # for customer in customers:
-    #     obj += length(customer.location, facilities[solution[customer.index]].location)
+    # obj += length(customer.location, facilities[solution[customer.index]].location)
 
     objective = pulp.LpAffineExpression(obj)
     facility.setObjective(objective)
 
-    facility.solve(pulp.PULP_CBC_CMD(maxSeconds= 1000))
+    facility.solve(pulp.PULP_CBC_CMD(maxSeconds=1000))
 
     out = []
     for f in f_set:
-            if used[f].value()>0.5:
-                #print ("col" + str(color) + "node" + str(node))
-                out.append(f)
-    #print out
+        if used[f].value() > 0.5:
+            # print ("col" + str(color) + "node" + str(node))
+            out.append(f)
+    # print out
     return out
+
 
 def solve_it(input_data):
     # Modify this code to run your optimization algorithm
@@ -168,18 +286,18 @@ def solve_it(input_data):
     parts = lines[0].split()
     facility_count = int(parts[0])
     customer_count = int(parts[1])
-    
+
     facilities = []
-    for i in range(1, facility_count+1):
+    for i in range(1, facility_count + 1):
         parts = lines[i].split()
-        facilities.append(Facility(i-1, float(parts[0]), int(parts[1]), Point(float(parts[2]), float(parts[3])) ))
+        facilities.append(Facility(i - 1, float(parts[0]), int(parts[1]), Point(float(parts[2]), float(parts[3]))))
 
     customers = []
-    for i in range(facility_count+1, facility_count+1+customer_count):
+    for i in range(facility_count + 1, facility_count + 1 + customer_count):
         parts = lines[i].split()
-        customers.append(Customer(i-1-facility_count, int(parts[0]), Point(float(parts[1]), float(parts[2]))))
+        customers.append(Customer(i - 1 - facility_count, int(parts[0]), Point(float(parts[1]), float(parts[2]))))
 
-    solution, used = greedy_solution(customers, facilities)
+    solution, used = local_greedy(customers, facilities)
 
     obj = solution_cost(customers, facilities, solution, used)
 
--- vrp/solver.py
+++ vrp/solver.py
@@ -3,11 +3,67 @@
 
 import math
 from collections import namedtuple
+import pulp
 
 Customer = namedtuple("Customer", ['index', 'demand', 'x', 'y'])
 
 def length(customer1, customer2):
     return math.sqrt((customer1.x - customer2.x)**2 + (customer1.y - customer2.y)**2)
+
+
+def trivial_solution(customers, vehicle_capacity, vehicle_count):
+    depot = customers[0]
+    # build a trivial solution
+    # assign customers to vehicles starting by the largest customer demands
+    vehicle_tours = []
+    remaining_customers = set(customers)
+    remaining_customers.remove(depot)
+    for v in range(0, vehicle_count):
+        # print "Start Vehicle: ",v
+        vehicle_tours.append([])
+        capacity_remaining = vehicle_capacity
+        while sum([capacity_remaining >= customer.demand for customer in remaining_customers]) > 0:
+            used = set()
+            order = sorted(remaining_customers, key=lambda customer: -customer.demand)
+            for customer in order:
+                if capacity_remaining >= customer.demand:
+                    capacity_remaining -= customer.demand
+                    vehicle_tours[v].append(customer)
+                    # print '   add', ci, capacity_remaining
+                    used.add(customer)
+            remaining_customers -= used
+
+    return vehicle_tours
+
+
+def cost(depot, vehicle_count, vehicle_tours):
+    obj = 0
+    for v in range(0, vehicle_count):
+        vehicle_tour = vehicle_tours[v]
+        if len(vehicle_tour) > 0:
+            obj += length(depot, vehicle_tour[0])
+            for i in range(0, len(vehicle_tour) - 1):
+                obj += length(vehicle_tour[i], vehicle_tour[i + 1])
+            obj += length(vehicle_tour[-1], depot)
+
+    return obj
+
+def build_variable(node_in,node_out):
+    if node_in > node_out:
+        return pulp.LpVariable("x_in" + str(node_in) + "_out" + str(node_out) , 0,1, 'Binary')
+    else:
+        return 0.0
+
+def pulp_solution(points,node_count):
+    model = pulp.LpProblem("Model", pulp.LpMinimize)
+    node_set = range(0,node_count)
+    out_edges =  [[build_variable(node_in,node_out) for node_in in node_set] for node_out in node_set]
+    model+= sum([sum([out_edges[node_in][node_out]*length(points[node_in], points[node_out]) for node_in in node_set ]) for node_out in node_set])
+
+    for node in node_set:
+        model += sum(out_edges[node][v] for v in node_set) + sum(out_edges[v][node] for v in node_set) == 2
+    model.solve()
+
 
 def solve_it(input_data):
     # Modify this code to run your optimization algorithm
@@ -27,43 +83,15 @@ def solve_it(input_data):
         customers.append(Customer(i-1, int(parts[0]), float(parts[1]), float(parts[2])))
 
     #the depot is always the first customer in the input
-    depot = customers[0] 
+    depot = customers[0]
 
-
-    # build a trivial solution
-    # assign customers to vehicles starting by the largest customer demands
-    vehicle_tours = []
-    
-    remaining_customers = set(customers)
-    remaining_customers.remove(depot)
-    
-    for v in range(0, vehicle_count):
-        # print "Start Vehicle: ",v
-        vehicle_tours.append([])
-        capacity_remaining = vehicle_capacity
-        while sum([capacity_remaining >= customer.demand for customer in remaining_customers]) > 0:
-            used = set()
-            order = sorted(remaining_customers, key=lambda customer: -customer.demand)
-            for customer in order:
-                if capacity_remaining >= customer.demand:
-                    capacity_remaining -= customer.demand
-                    vehicle_tours[v].append(customer)
-                    # print '   add', ci, capacity_remaining
-                    used.add(customer)
-            remaining_customers -= used
+    vehicle_tours = trivial_solution(customers, vehicle_capacity, vehicle_count)
 
     # checks that the number of customers served is correct
     assert sum([len(v) for v in vehicle_tours]) == len(customers) - 1
 
     # calculate the cost of the solution; for each vehicle the length of the route
-    obj = 0
-    for v in range(0, vehicle_count):
-        vehicle_tour = vehicle_tours[v]
-        if len(vehicle_tour) > 0:
-            obj += length(depot,vehicle_tour[0])
-            for i in range(0, len(vehicle_tour)-1):
-                obj += length(vehicle_tour[i],vehicle_tour[i+1])
-            obj += length(vehicle_tour[-1],depot)
+    obj = cost(depot, vehicle_count, vehicle_tours)
 
     # prepare the solution in the specified output format
     outputData = str(obj) + ' ' + str(0) + '\n'
